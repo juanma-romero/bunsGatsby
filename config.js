e46584fb6096a7729e46f51c0e5f28c26e055e39
Juanma Romero <xjuanma.romerox@gmail.com>
2020-03-15 13:25:23 -0300

    tercer cambios varios

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Buns artesanal', // Site title.
+  siteTitleAlt: 'Buns panes y pizas artesanales', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Buns panes artesanales y pre pizzas. Venta a comercios.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
+  disqusShortname: 'buns', // Disqus shortname.
   userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  userTwitter: '',
+  userLocation: 'Buenos Aires, Argentina',
   userDescription: '',
   copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
--- src/html.js
+++ src/html.js
@@ -10,6 +10,7 @@ export default class HTML extends Component {
           <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no' />
           {this.props.headComponents}
           <link rel='shortcut icon' href={favicon} />
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </head>
         <body>
           <div
@@ -17,6 +18,17 @@ export default class HTML extends Component {
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
           {this.props.postBodyComponents}
+          <script>
+            if (window.netlifyIdentity) {
+              window.netlifyIdentity.on("init", user => {
+                if (!user) {
+                  window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                  });
+                }
+              })
+            }
+          </script>
         </body>
       </html>
     )
